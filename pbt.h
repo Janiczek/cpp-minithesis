998787df7bf9064587e4d6448738f951d61e1b0e
Martin Janiczek <martin@janiczek.cz>
2023-08-27 00:53:43 +0200

    Format stuff

--- pbt.h
+++ pbt.h
@@ -10,7 +10,7 @@
 #include <variant>
 #include <vector>
 
-#define MAX_RANDOMRUN_LENGTH (64 * 1024) // 64 k items
+#define MAX_RANDOMRUN_LENGTH (64 * 1024)// 64 k items
 #define MAX_GENERATED_VALUES_PER_TEST 100
 #define MAX_GEN_ATTEMPTS_PER_VALUE 15
 #define RAND_TYPE unsigned int
@@ -38,6 +38,7 @@ public:
     [[nodiscard]] bool is_full() const { return run.size() >= MAX_RANDOMRUN_LENGTH; }
     void push_back(RAND_TYPE n) { run.push_back(n); }
     RAND_TYPE next() { return *it++; }
+
 private:
     std::vector<RAND_TYPE> run;
     std::vector<RAND_TYPE>::const_iterator it = run.cbegin();
@@ -46,48 +47,53 @@ private:
 // RandSource
 
 struct Live {
-    RandomRun run; // in the process of being created
+    RandomRun run;// in the process of being created
     std::mt19937 &rng;
 };
 struct Recorded {
-    RandomRun run; // in the process of being consumed
+    RandomRun run;// in the process of being consumed
 };
 using RandSource = std::variant<Live, Recorded>;
 
 RandomRun random_run(RandSource rand) {
-    struct getter
-    {
-        RandomRun operator()(const Live& l)     { return l.run; }
-        RandomRun operator()(const Recorded& r) { return r.run; }
+    struct getter {
+        RandomRun operator()(const Live &l) { return l.run; }
+        RandomRun operator()(const Recorded &r) { return r.run; }
     };
     return std::visit(getter{}, rand);
 }
 
 // GenResult
 
-template <typename T> struct Generated {
-    RandomRun run; // run corresponding to the value
+template<typename T>
+struct Generated {
+    RandomRun run;// run corresponding to the value
     T value;
 };
 struct Rejected {
     std::string reason;
 };
-template <typename T> using GenResult = std::variant<Generated<T>, Rejected>;
-template <typename T> GenResult<T> generated(RandomRun run, T val) {
+template<typename T>
+using GenResult = std::variant<Generated<T>, Rejected>;
+template<typename T>
+GenResult<T> generated(RandomRun run, T val) {
     return GenResult<T>{Generated<T>{run, val}};
 }
-template <typename T> GenResult<T> generated(T val) {
+template<typename T>
+GenResult<T> generated(T val) {
     return generated(RandomRun(), val);
 }
-template <typename T> GenResult<T> rejected(std::string reason) {
+template<typename T>
+GenResult<T> rejected(std::string reason) {
     return GenResult<T>{Rejected{std::move(reason)}};
 }
 
 // Generator
 
-template <typename T> class Generator {
+template<typename T>
+class Generator {
 public:
-    using FunctionType = std::function<GenResult<T>(RandSource const&)>;
+    using FunctionType = std::function<GenResult<T>(RandSource const &)>;
 
     explicit Generator(FunctionType function) : fn(std::move(function)) {}
 
@@ -103,67 +109,73 @@ private:
 
 namespace Gen {
 
-    template <typename T> Generator<T> constant(T const& val) {
-        return Generator<T>([val](RandSource const&) {
+    template<typename T>
+    Generator<T> constant(T const &val) {
+        return Generator<T>([val](RandSource const &) {
             return generated(val);
         });
     }
     Generator<unsigned int> unsigned_int(unsigned int max) {
-        return Generator<unsigned int>([max](RandSource const& rand) {
-          if (random_run(rand).is_full()) {
-              return rejected<unsigned int>("Generators have hit maximum RandomRun length (generating too much data).");
-          }
-          struct handler {
-              unsigned int max_value;
-              explicit handler(unsigned int max) : max_value(max) {}
+        return Generator<unsigned int>([max](RandSource const &rand) {
+            if (random_run(rand).is_full()) {
+                return rejected<unsigned int>("Generators have hit maximum RandomRun length (generating too much data).");
+            }
+            struct handler {
+                unsigned int max_value;
+                explicit handler(unsigned int max) : max_value(max) {}
 
-              GenResult<unsigned int> operator()(Live l) const {
-                  std::uniform_int_distribution<unsigned int> dist(0,max_value);
-                  auto val = dist(l.rng);
-                  l.run.push_back(val);
-                  return generated(l.run,val);
-              }
-              GenResult<unsigned int> operator()(Recorded r) {
-                  if (r.run.is_empty()) {
-                      return rejected<unsigned int>("Ran out of recorded bits");
-                  }
-                  auto val = r.run.next();
-                  return generated(r.run, val);
-              }
-          };
-          return std::visit(handler{max}, rand);
+                GenResult<unsigned int> operator()(Live l) const {
+                    std::uniform_int_distribution<unsigned int> dist(0, max_value);
+                    auto val = dist(l.rng);
+                    l.run.push_back(val);
+                    return generated(l.run, val);
+                }
+                GenResult<unsigned int> operator()(Recorded r) {
+                    if (r.run.is_empty()) {
+                        return rejected<unsigned int>("Ran out of recorded bits");
+                    }
+                    auto val = r.run.next();
+                    return generated(r.run, val);
+                }
+            };
+            return std::visit(handler{max}, rand);
         });
     }
 
-}
+}// namespace Gen
 
 
 // TestResult
 
 struct Passes {};
-template <typename T> struct FailsWith {
+template<typename T>
+struct FailsWith {
     T value;
     std::string error;
 };
-struct CannotGenerateValues { std::map<std::string,int> rejections; };
-template <typename T> using TestResult = std::variant<Passes,FailsWith<T>,CannotGenerateValues>;
+struct CannotGenerateValues {
+    std::map<std::string, int> rejections;
+};
+template<typename T>
+using TestResult = std::variant<Passes, FailsWith<T>, CannotGenerateValues>;
 
-template <typename T>
-std::string to_string(const TestResult<T>& result) {
+template<typename T>
+std::string to_string(const TestResult<T> &result) {
     struct stringifier {
-        std::string operator()(Passes _)                        { return "Passes"; }
-        std::string operator()(FailsWith<T> f)                  { return "Fails: \"" + f.error + "\""; } // TODO show the value
-        std::string operator()(const CannotGenerateValues& cgv) { return "Cannot generate values. Most common reason: "
-                                                                         + std::max_element(cgv.rejections.begin(),
-                                                                                            cgv.rejections.end()
-                                                                                            )->first; } // TODO show more rejections
+        std::string operator()(Passes _) { return "Passes"; }
+        std::string operator()(FailsWith<T> f) { return "Fails: \"" + f.error + "\""; }// TODO show the value
+        std::string operator()(const CannotGenerateValues &cgv) {
+            // TODO show top 5 reasons
+            auto most_common_reason = std::max_element(cgv.rejections.begin(), cgv.rejections.end())->first;
+            return "Cannot generate values. Most common reason: " + most_common_reason;
+        }
     };
     return std::visit(stringifier{}, result);
 }
 
 // Runner
 
-template <typename T, typename FN>
+template<typename T, typename FN>
 TestResult<T> run(Generator<T> generator, FN testFunction) {
 
     RandomRun emptyRun;
@@ -172,7 +184,7 @@ TestResult<T> run(Generator<T> generator, FN testFunction) {
     Live liveSource{emptyRun, rng};
 
     for (int i = 0; i < MAX_GENERATED_VALUES_PER_TEST; i++) {
-        std::map<std::string,int> rejections;
+        std::map<std::string, int> rejections;
         bool generated_successfully = false;
         for (int gen_attempt = 0; gen_attempt < MAX_GEN_ATTEMPTS_PER_VALUE && !generated_successfully; gen_attempt++) {
             GenResult<T> genResult = generator.run(liveSource);
@@ -187,7 +199,7 @@ TestResult<T> run(Generator<T> generator, FN testFunction) {
                 rejections[rejected->reason]++;
             }
         }
-        if (!generated_successfully) { // meaning the loop above got to the full MAX_GEN_ATTEMPTS_PER_VALUE and gave up
+        if (!generated_successfully) {// meaning the loop above got to the full MAX_GEN_ATTEMPTS_PER_VALUE and gave up
             return CannotGenerateValues{rejections};
         }
     }
